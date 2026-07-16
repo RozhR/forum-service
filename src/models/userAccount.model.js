f294a71eb4f84eb24bd54d15a43ed548fb02e2cf
RozhR <rozhroman@gmail.com>
2026-07-16 23:26:21 +0300

    Implement authentication middleware and enhance user account features

--- src/models/userAccount.model.js
+++ src/models/userAccount.model.js
@@ -1,5 +1,6 @@
 import {Schema, model} from 'mongoose';
 import {USER} from "../configuration/constants.js";
+import bcrypt from "bcrypt";
 
 const userAccountSchema = new Schema({
     _id: {
@@ -10,7 +11,7 @@ const userAccountSchema = new Schema({
     password: {
         type: String,
         required: true,
-        select: false
+
     },
     firstName: {
         type: String,
@@ -34,5 +35,14 @@ const userAccountSchema = new Schema({
         }
     }
 })
+
+userAccountSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+userAccountSchema.methods.comparePassword = async function (plainTextPassword) {
+    return bcrypt.compare(plainTextPassword, this.password);
+}
 
 export default model('UserAccount', userAccountSchema, 'users');
