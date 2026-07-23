35e02d40dde11cd5ac3378852e32d10009ce6260
RozhR <rozhroman@gmail.com>
2026-07-23 18:10:45 +0300

    Add isOwnerOrHasRole middleware and DELETE route for enhanced authorization

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,3 +1,15 @@
-export const hasRole = role => (req, res, next) => req.principal.roles.includes(role.toUpperCase().trim())  ? next() : res.status(403).json({message: 'Access denied'});
+export const hasRole = role => (req, res, next) =>
+    req.principal.roles.includes(role.toUpperCase().trim())
+        ? next() : res.status(403).json({message: 'Access denied'});
 
-export const isOwner = paramName => (req, res, next) => req.params[paramName] === req.principal.userName ? next() : res.status(403).json({message: 'Access denied'});
+export const isOwner = paramName => (req, res, next) =>
+    req.params[paramName] === req.principal.userName
+        ? next() : res.status(403).json({message: 'Access denied'});
+
+export const isOwnerOrAdmin = paramName => (req, res, next) => {
+    const isOwner = req.params[paramName] === req.principal.userName;
+    const isAdmin = req.principal.roles.includes(ADMIN);
+
+    return isOwner || isAdmin
+        ? next() : res.status(403).json({message: 'Access denied'});
+};
--- src/routes/authorization.routes.js
+++ src/routes/authorization.routes.js
@@ -1,10 +1,13 @@
 import {Router} from "express";
-import {hasRole, isOwner} from "../middlewares/authorization.middleware.js";
+import {hasRole, isOwner, isOwnerOrHasRole} from "../middlewares/authorization.middleware.js";
 import {ADMIN} from "../configuration/constants.js";
 
 const router = Router();
 
 router.all('/account/user/:login/role/:role', hasRole(ADMIN));
 router.patch('/account/user/:user', isOwner('user'));
+router.delete('/account/user/:login', isOwnerOrHasRole('login', ADMIN));
+router.post('/forum/post/:author', isOwner('author'));
+router.patch('/forum/post/:id/comment/:user', isOwner('user'));
 
 export default router;
