d591012df2d26b23a56d38331a91621331299ded
RozhR <rozhroman@gmail.com>
2026-07-23 18:47:19 +0300

    Add isPostAuthor middleware and secure /forum/post/:id PATCH route with author-based access control

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,15 +1,17 @@
-export const hasRole = role => (req, res, next) =>
-    req.principal.roles.includes(role.toUpperCase().trim())
-        ? next() : res.status(403).json({message: 'Access denied'});
+import {getPostById} from "../services/post.service.js";
 
-export const isOwner = paramName => (req, res, next) =>
-    req.params[paramName] === req.principal.userName
-        ? next() : res.status(403).json({message: 'Access denied'});
+export const hasRole = role => (req, res, next) => req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
 
-export const isOwnerOrAdmin = paramName => (req, res, next) => {
+export const isOwner = paramName => (req, res, next) => req.params[paramName] === req.principal.userName ? next() : res.status(403).json({message: 'Access denied'});
+
+export const isOwnerOrHasRole = (paramName, role) => (req, res, next) => {
     const isOwner = req.params[paramName] === req.principal.userName;
-    const isAdmin = req.principal.roles.includes(ADMIN);
+    const hasRole = req.principal.roles.includes(role.toUpperCase().trim());
+    return isOwner || hasRole ? next() : res.status(403).json({message: 'Access denied'});
+}
 
-    return isOwner || isAdmin
-        ? next() : res.status(403).json({message: 'Access denied'});
-};
+export const isPostAuthor = (postIdParam = 'id') => async (req, res, next) => {
+    const postId = req.params[postIdParam];
+    const post = await getPostById(postId);
+    return post.author === req.principal.userName ? next() : res.status(403).json({message: 'Access denied'});
+}
